5dd0930fdac7fb44dde68472360a17ff2dbd293b
Hameedullah Pardess <hameedullah.pardess@gmail.com>
2016-11-27 21:59:41 +0430

    LaraTrust package seeds are setup

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(LaratrustSeeder::class);
+        
         DB::table('users')->insert([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
