9012a24e5ddf790ccb324d7a58b560aeeb2e98d1
Hameedullah Pardess <hameedullah.pardess@yahoo.com>
2016-11-27 17:09:38 +0430

    LaraTrust package is added
    https://github.com/santigarcor/laratrust
    https://santigarcor.gitbooks.io/laratrust-docs/content/installation.html

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Laratrust
+        Laratrust\LaratrustServiceProvider::class,
     ],
 
     /*
@@ -226,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Laratrust'   => Laratrust\LaratrustFacade::class,
     ],
 
 ];
